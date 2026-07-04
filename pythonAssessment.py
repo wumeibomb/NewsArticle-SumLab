455b6d85e99e45c64f1e2230a7d8be5062cdbf8b
Amina Masera <aminakavele@gmail.com>
2026-07-05 00:48:12 +0400

    calculate_average_word_length - worksgit add pythonAssessment.py !

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -1,7 +1,6 @@
 import re
 
-text_to_analyze = "With the break break statement we can stop the loop before it has looped through all the items, break in the statment break"
-#my test string ^
+text_to_analyze = "With the dhvbwh vsjdvs sdijnvsjk sdfwknv sd vbweif vkdbvhiw d viwbv whvb wdv iwb vkd icbad visbdv wi vsd vob vn iv wknv whi vwnbvwd viw v sbvsihvb wvwi vm vbreak break statement we can stop the loop before it has looped through all the items, break in the statment break"
 
 #word2 = input("word to count: ")
 
@@ -23,17 +22,12 @@ def count_specific_word(text, word2):
 def calculate_average_word_length(str):
     mean = text_to_analyze.split()
     count = 0
-    unwanted_characters = 0
-    #pattern = r"^[a-zA-Z(+*)]"
-    #bjjb = re.match(pattern, str)
+    pattern = r"\w" #how do I exclude the underscore??
+    bjjb = re.findall(pattern, str)
 
-    for eachletter in str:
-        if not eachletter == " " and ",":
-            count += 1
-        else:
-            unwanted_characters += 1
-        
-    print(count, unwanted_characters)
+    for eachcharacter in bjjb:
+         count += 1
+
     average = count / len(mean)
     print(average.__round__(2))
 
